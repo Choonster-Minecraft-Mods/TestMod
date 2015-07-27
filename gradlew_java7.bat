3486a8849e37f60656016699e6ce6c2e8e201515
Choonster <Choonster.2010@gmail.com>
2015-07-27 22:22:50 +1000

    Update JDK version in Java 7 Gradle Wrapper

--- gradlew_java7.bat
+++ gradlew_java7.bat
@@ -3,7 +3,7 @@
 rem Sets JAVA_HOME to JDK 7 before launching Gradle because ForgeGradle 1.0 is incompatible with JDK 8
 
 setlocal
-set "JAVA_HOME=C:\Program Files\Java\jdk1.7.0_79"
+set "JAVA_HOME=C:\Program Files\Java\jdk1.7.0_80"
 
 ./gradlew.bat %*
 
